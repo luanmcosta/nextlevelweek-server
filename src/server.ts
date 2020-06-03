068cf30cfd934727adb6a29f9c74f3842b3f0f8b
Luan Mineu Costa <luanwebdev@gmail.com>
2020-06-03 00:10:59 -0300

    Atualização do Servidor

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,20 @@ import express from 'express'
 const app = express();
 
 app.get('/users', (request, response) => {
-    let o = {
-        nome: 'Costa',
-        idade: 22
-    }
-    response.json(o)
+    response.json([
+        'Luan',
+        'Carlos',
+        'Alfred'
+    ])
 });
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Diego',
+        email: 'luanwebdev@gmail.com'
+    }
+
+    return response.json(user)
+})
 
 app.listen(3333);
